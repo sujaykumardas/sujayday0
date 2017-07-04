f45699cffff7f790b896c5e6a0e6b5a7541746fa
sujaykumardas <sdas@grepruby.com>
2017-07-04 13:48:27 +0530

    jsbasics

--- js/factorial.js
+++ js/factorial.js
@@ -1,6 +1,6 @@
 var welmsg;
 welmsg="welcome to factorial programming"
-alert(welmsg); 
+document.writeln(welmsg); 
 
 var i, no, fact;
 no=5;
@@ -11,9 +11,10 @@ for(i=1; i<=no; i++)
 fact= fact*i;
 } 
 
-alert("factorial of 5 is"+fact); 
+document.writeln(fact); 
+
  welmsg="welcome to reversing of string"
-alert(welmsg); 
+document.writeln(welmsg); 
 
 var str=['sujay','kumar','das'];
 var rev=[];
@@ -23,5 +24,5 @@ for(i=0;i<no;i++)
 	rev[no-i-1]=str[i];
 }
 	
-alert(rev);
+document.writeln(rev); 
 
