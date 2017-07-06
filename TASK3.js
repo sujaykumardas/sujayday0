6cef351f67c2a97e48ea30378d594ad76ee53e1d
sujaykumardas <sdas@grepruby.com>
2017-07-06 16:55:25 +0530

    day3: TASK3 Printing names etc

--- TASK3.js
+++ TASK3.js
@@ -3,7 +3,7 @@
 var http = require("http");
 http.createServer(function (request, response) {
    
-   response.writeHead(200, {'Content-Type': 'text'});
+   
    response.end('NAME: SUJAY KUMAR DAS\n ADDRESS: 241, MANAVATA NAGAR\nCOLLEGE :SGSITS ');
 }).listen(81);
 
